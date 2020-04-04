977946b20456061dd8b881c4e3f3d096f8362cd5
Mac Macoy <mamacoy907@gmail.com>
2020-04-04 12:27:02 -0400

    access camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ar-messenger';
+
+  @ViewChild('video', { static: true }) videoElement: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.startCamera();
+  }
+
+  constraints = {
+    video: {
+        facingMode: "environment",
+        width: { ideal: 4096 },
+        height: { ideal: 2160 }
+    }
+  };
+
+  startCamera() {
+    if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) { 
+      navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
+    } else {
+        alert('Sorry, camera not available.');
+    }
+  }
+
+  attachVideo(stream) {
+    this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+  }
+
+  handleError(error) {
+    console.log('Error: ', error);
+  }
+
 }
