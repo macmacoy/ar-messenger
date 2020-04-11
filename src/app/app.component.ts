b539cda530b5d6aaa4aae52ceca21a6466a0ce85
Mac Macoy <mamacoy907@gmail.com>
2020-04-11 11:59:27 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ export class AppComponent implements OnInit {
 
   title = 'ar-messenger';
 
-  locationPicUrl = "https://i.kym-cdn.com/entries/icons/facebook/000/016/894/mynameehhjeff.jpg";
-  usdzUrl = "https://github.com/macmacoy/ar-messenger/blob/master/src/assets/toy_drummer.usdz?raw=true";
+  locationPicUrl = "https://contentgrid.homedepot-static.com/hdus/en_US/DTCCOMNEW/fetch/Category_Pages/Kitchen/Countertops_and_Backsplashes/countertops-hero-mob3.jpg";
+  usdzUrl = "https://developer.apple.com/augmented-reality/quick-look/models/biplane/toy_biplane.usdz";
 
   constructor() {}
 
