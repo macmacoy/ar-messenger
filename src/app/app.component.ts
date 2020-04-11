bb6ea297fbdc4e2c1c41cc44bc4be986ee7a1e18
Mac Macoy <mamacoy907@gmail.com>
2020-04-11 13:30:10 -0500

    hide address bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,28 @@ export class AppComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.hideToolBar();
+  }
+
+  hideToolBar() {
+    if (!window.location.hash && window.addEventListener){
+      window.addEventListener("load", function() {
+          setTimeout(function(){
+              window.scrollTo(0, 0);
+          }, 0);
+      });
+      window.addEventListener( "orientationchange",function() {
+          setTimeout(function(){
+              window.scrollTo(0, 0);
+          }, 0);
+      });
+      window.addEventListener( "touchstart",function() {
+           setTimeout(function(){
+               window.scrollTo(0, 0);
+           }, 0);
+       });
+    }
+  }
 
 }
