5962a5db99b7818bf1535bec8ae5a8d83b3c8c1f
Mac Macoy <mamacoy907@gmail.com>
2020-04-11 12:18:32 -0500

    assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent implements OnInit {
   title = 'ar-messenger';
 
   locationPicUrl = "https://contentgrid.homedepot-static.com/hdus/en_US/DTCCOMNEW/fetch/Category_Pages/Kitchen/Countertops_and_Backsplashes/countertops-hero-mob3.jpg";
-  usdzUrl = "https://developer.apple.com/augmented-reality/quick-look/models/biplane/toy_biplane.usdz";
+  // usdzUrl = "https://developer.apple.com/augmented-reality/quick-look/models/biplane/toy_biplane.usdz";
+  usdzUrl = "/assets/toy_drummer.usdz";
   openCameraButtonUrl = "https://i0.wp.com/css-tricks.com/wp-content/uploads/2018/05/button.jpg?ssl=1";
 
   constructor() {}
