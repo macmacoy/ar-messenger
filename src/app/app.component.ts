b9d6693c1b2de0fb31d53ba1907d602a65e48329
Mac Macoy <mamacoy907@gmail.com>
2020-04-11 12:44:38 -0500

    button image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   locationPicUrl = "https://contentgrid.homedepot-static.com/hdus/en_US/DTCCOMNEW/fetch/Category_Pages/Kitchen/Countertops_and_Backsplashes/countertops-hero-mob3.jpg";
   // usdzUrl = "https://developer.apple.com/augmented-reality/quick-look/models/biplane/toy_biplane.usdz";
   usdzUrl = "https://macmacoy.github.io/ar-messenger/assets/toy_drummer.usdz";
-  openCameraButtonUrl = "https://i0.wp.com/css-tricks.com/wp-content/uploads/2018/05/button.jpg?ssl=1";
+  openCameraButtonUrl = "https://macmacoy.github.io/ar-messenger/assets/OpenCameraButton.png";
 
   constructor() {}
 
