ef15d6eca39da2c860686ce3bc389f42ec1c9598
Mac Macoy <mamacoy907@gmail.com>
2020-05-25 15:10:05 -0400

    open camera button

--- src/app/view-message-attempt/view-message-attempt.component.ts
+++ src/app/view-message-attempt/view-message-attempt.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageServiceService } from '../message-service/message-service.service';
 import { Message } from '../message-service/message-service.service';
+import { LocationServiceService } from '../location-service/location-service.service';
 
 @Component({
   selector: 'app-view-message-attempt',
@@ -9,11 +10,18 @@ import { Message } from '../message-service/message-service.service';
 })
 export class ViewMessageAttemptComponent implements OnInit {
 
+  // approximately a quarter mile
+  maxLongitudeDiff = 0.004;
+  maxLatitudeDiff = 0.004;
+
   inRange = true;
   messageId: string;
   message: Message;
 
-  constructor(private messageService: MessageServiceService) {}
+  constructor(
+    private messageService: MessageServiceService,
+    private locationService: LocationServiceService
+  ) {}
 
   ngOnInit(): void {
     this.setMessage();
@@ -22,6 +30,18 @@ export class ViewMessageAttemptComponent implements OnInit {
   setMessage() {
     this.messageService.getMessage(this.messageId).subscribe(message => {
       this.message = message;
+      this.setInRange();
+    });
+  }
+
+  setInRange() {
+    this.locationService.getPosition().then(pos => {
+      this.inRange = (
+        Math.abs(+this.message.longitude - +pos.lng) < this.maxLongitudeDiff &&
+        Math.abs(+this.message.latitude - +pos.lat) < this.maxLatitudeDiff
+      );
+      console.log(String(pos.lng));
+      console.log(String(pos.lat));
     });
   }
 
